584f51133cd45d3d97c89cb403434b256ea52669
Linkin143 <kumarprince143p143@gmail.com>
2026-02-22 00:38:43 +0530

    no change

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,4 +1,4 @@
-import Contact from '../models/Contact.js';
+import Contact from '../models/contact.js';
 import { userThanksTemplate } from '../utils/emailTemplates.js';
 import { sendEmail } from '../utils/sendEmail.js';
 

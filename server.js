c45430844dcd86b37b107d184f4b92d715f5e103
Linkin143 <kumarprince143p143@gmail.com>
2026-02-21 18:59:13 +0530

    feat: server integration

---
+++ server.js
@@ -0,0 +1,20 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import connectDB from './config/db.js';
+import contactRoutes from './routes/contactRoutes.js';
+import testimonialRoutes from './routes/testimonialRoutes.js';
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/testimonials', testimonialRoutes);
+app.use('/api/contact', contactRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

158e4c512f0505cfb9ed14432f7528df82add6c9
Linkin143 <kumarprince143p143@gmail.com>
2026-02-22 01:49:17 +0530

    feat: slash removed in cors origin

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB();
 
 const app = express();
 app.use(cors({
-  origin: ["https://aevrix.netlify.app/", "http://localhost:8080"],
+  origin: ["https://aevrix.netlify.app", "http://localhost:8080"],
   methods: ["GET", "POST"]
 }));
 app.use(express.json());

ecb39d8e3af57a0012025ff3219057a06554ce03
Linkin143 <kumarprince143p143@gmail.com>
2026-02-22 21:27:22 +0530

    testing user acknowledgement mail

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,4 +1,5 @@
 import Contact from '../models/contact.js';
+import { userThanksTemplate } from '../utils/emailTemplates.js';
 import { sendEmail } from '../utils/sendEmail.js';
 
 export const submitContactForm = async (req, res) => {
@@ -31,13 +32,9 @@ export const submitContactForm = async (req, res) => {
 
     // 3. Email to User (Auto-response)
     await sendEmail({
-      email: email,
+      email: process.env.FOUNDER_EMAIL,
       subject: 'Thanks for reaching out to AEVRIX',
-      // html: userThanksTemplate(name),
-      html: `<p>Hi ${name},</p>
-           <p>Thanks for reaching out to AEVRIX AI.</p>
-           <p>Our team has received your message and will get back to you shortly.</p>
-           <p>— AEVRIX AI Team</p>`,
+      html: userThanksTemplate(name),
     });
 
     res.status(201).json({ success: true, message: "Message sent successfully" });
